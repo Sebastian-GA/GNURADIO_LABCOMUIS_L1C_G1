03ccbf2463bc556315e5d4edb39d31139661037e
Sebastian-GA <isebastian.ga@gmail.com>
2022-09-09 18:32:15 -0500

    practica 4.1 - modulacionAM

---
+++ Lab4 - Modulaciones de Amplitud/Lab4.1 - Modulaciones de Amplitud en Doble Banda Lateral/README.md
@@ -0,0 +1,21 @@
+# Lab4.1 - Modulacion de Amplitud en Doble Banda Lateral
+
+## Modulacion AM
+
+- ka*Am < 1
+
+![Tiempo](https://user-images.githubusercontent.com/62948474/189462003-90a16430-eb0e-4ca7-93fa-7f00e0ab080b.jpeg)
+![Frecuencia](https://user-images.githubusercontent.com/62948474/189462258-1546174e-c368-496d-bb22-15e96eeb1d17.jpeg)
+![GNU](https://user-images.githubusercontent.com/62948474/189462266-d99a5195-fa46-4da5-b534-024d378b1e86.png)
+
+- ka*Am = 1
+
+![Tiempo](https://user-images.githubusercontent.com/62948474/189462296-c76f0d1d-8610-462d-b46d-90a1c4ce0b36.jpeg)
+![Frecuencia](https://user-images.githubusercontent.com/62948474/189462293-8320d985-bf8d-468a-a33d-7240a583e160.jpeg)
+![GNU](https://user-images.githubusercontent.com/62948474/189462295-47d94b4f-d387-4859-a376-7f48c941cfff.png)
+
+- ka*Am > 1
+
+![Tiempo](https://user-images.githubusercontent.com/62948474/189462325-70d0f647-5162-437f-90c2-24f8e3885755.jpeg)
+![Frecuencia](https://user-images.githubusercontent.com/62948474/189462323-ef0f2ed9-5a96-4c0d-851f-19570912e7a4.jpeg)
+![GNU](https://user-images.githubusercontent.com/62948474/189462324-62e5e657-67c7-4613-8ab7-cca716239dac.png)
--- Lab4 - Modulaciones de Amplitud/Lab4.1 - Modulaciones de Amplitud en Doble Banda Lateral/modulacionAM.py
+++ Lab4 - Modulaciones de Amplitud/Lab4.1 - Modulaciones de Amplitud en Doble Banda Lateral/modulacionAM.py
@@ -5,12 +5,11 @@
 # SPDX-License-Identifier: GPL-3.0
 #
 # GNU Radio Python Flow Graph
-# Title: MODULADORAM
+# Title: Modulacion AM
 # Author: Sebastian - Esteban
-# Copyright: UIS
-# GNU Radio version: 3.9.5.0
+# GNU Radio version: 3.10.2.0
 
-from distutils.version import StrictVersion
+from packaging.version import Version as StrictVersion
 
 if __name__ == '__main__':
     import ctypes
@@ -26,6 +25,7 @@ import os
 import sys
 sys.path.append(os.environ.get('GRC_HIER_PATH', os.path.expanduser('~/.grc_gnuradio')))
 
+from ModuladorAM import ModuladorAM  # grc-generated hier_block
 from PyQt5 import Qt
 from gnuradio import qtgui
 from gnuradio.filter import firdes
@@ -37,22 +37,19 @@ import signal
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
-from gnuradio import uhd
-import time
 from gnuradio.qtgui import Range, RangeWidget
 from PyQt5 import QtCore
-from moduladorAMV2 import moduladorAMV2  # grc-generated hier_block
 
 
 
 from gnuradio import qtgui
 
-class MODULADORAM(gr.top_block, Qt.QWidget):
+class modulacionAM(gr.top_block, Qt.QWidget):
 
     def __init__(self):
-        gr.top_block.__init__(self, "MODULADORAM", catch_exceptions=True)
+        gr.top_block.__init__(self, "Modulacion AM", catch_exceptions=True)
         Qt.QWidget.__init__(self)
-        self.setWindowTitle("MODULADORAM")
+        self.setWindowTitle("Modulacion AM")
         qtgui.util.check_set_qss()
         try:
             self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
@@ -70,7 +67,7 @@ class MODULADORAM(gr.top_block, Qt.QWidget):
         self.top_grid_layout = Qt.QGridLayout()
         self.top_layout.addLayout(self.top_grid_layout)
 
-        self.settings = Qt.QSettings("GNU Radio", "MODULADORAM")
+        self.settings = Qt.QSettings("GNU Radio", "modulacionAM")
 
         try:
             if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
@@ -83,50 +80,27 @@ class MODULADORAM(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
-        self.samp_rate = samp_rate = 200000
+        self.samp_rate = samp_rate = 200e3
         self.ka = ka = 1
-        self.fc = fc = 50
-        self.GTX = GTX = 0
-        self.Fm = Fm = 20e3
+        self.fm = fm = 20e3
+        self.Am = Am = 1
         self.Ac = Ac = 100e-3
-        self.A = A = 1
 
         ##################################################
         # Blocks
         ##################################################
         self._ka_range = Range(0, 4, 10e-3, 1, 200)
-        self._ka_win = RangeWidget(self._ka_range, self.set_ka, "coeficiente ka", "counter_slider", float, QtCore.Qt.Horizontal)
+        self._ka_win = RangeWidget(self._ka_range, self.set_ka, "Coeficiente ka", "counter_slider", float, QtCore.Qt.Horizontal)
         self.top_layout.addWidget(self._ka_win)
-        self._fc_range = Range(50, 2200, 100e-3, 50, 200)
-        self._fc_win = RangeWidget(self._fc_range, self.set_fc, "frecuencia portadora {MHz}", "counter_slider", float, QtCore.Qt.Horizontal)
-        self.top_layout.addWidget(self._fc_win)
-        self._GTX_range = Range(0, 31, 1, 0, 200)
-        self._GTX_win = RangeWidget(self._GTX_range, self.set_GTX, "ganancia TX", "counter_slider", float, QtCore.Qt.Horizontal)
-        self.top_layout.addWidget(self._GTX_win)
-        self._Fm_range = Range(0, 100e3, 1e3, 20e3, 200)
-        self._Fm_win = RangeWidget(self._Fm_range, self.set_Fm, "frecuencia del mensaje ", "counter_slider", float, QtCore.Qt.Horizontal)
-        self.top_layout.addWidget(self._Fm_win)
+        self._fm_range = Range(0, 100e3, 1e3, 20e3, 200)
+        self._fm_win = RangeWidget(self._fm_range, self.set_fm, "Frecuencia Mensaje", "counter_slider", float, QtCore.Qt.Horizontal)
+        self.top_layout.addWidget(self._fm_win)
+        self._Am_range = Range(0, 4, 10e-3, 1, 200)
+        self._Am_win = RangeWidget(self._Am_range, self.set_Am, "Amplitud Mensaje", "counter_slider", float, QtCore.Qt.Horizontal)
+        self.top_layout.addWidget(self._Am_win)
         self._Ac_range = Range(0, 4, 10e-3, 100e-3, 200)
-        self._Ac_win = RangeWidget(self._Ac_range, self.set_Ac, "Amplitud portadora", "counter_slider", float, QtCore.Qt.Horizontal)
+        self._Ac_win = RangeWidget(self._Ac_range, self.set_Ac, "Amplitud Portadora", "counter_slider", float, QtCore.Qt.Horizontal)
         self.top_layout.addWidget(self._Ac_win)
-        self._A_range = Range(0, 4, 10e-3, 1, 200)
-        self._A_win = RangeWidget(self._A_range, self.set_A, "Amplitud mensaje", "counter_slider", float, QtCore.Qt.Horizontal)
-        self.top_layout.addWidget(self._A_win)
-        self.uhd_usrp_sink_0 = uhd.usrp_sink(
-            ",".join(("", '')),
-            uhd.stream_args(
-                cpu_format="fc32",
-                args='',
-                channels=list(range(0,1)),
-            ),
-            "",
-        )
-        self.uhd_usrp_sink_0.set_samp_rate(samp_rate)
-        self.uhd_usrp_sink_0.set_time_now(uhd.time_spec(time.time()), uhd.ALL_MBOARDS)
-
-        self.uhd_usrp_sink_0.set_center_freq(fc*1e6, 0)
-        self.uhd_usrp_sink_0.set_antenna("TX/RX", 0)
-        self.uhd_usrp_sink_0.set_gain(GTX, 0)
         self.qtgui_time_sink_x_0 = qtgui.time_sink_c(
             1024, #size
             samp_rate, #samp_rate
@@ -220,24 +194,23 @@ class MODULADORAM(gr.top_block, Qt.QWidget):
 
         self._qtgui_freq_sink_x_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0.qwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_freq_sink_x_0_win)
-        self.moduladorAMV2_0 = moduladorAMV2(
+        self.analog_sig_source_x_0 = analog.sig_source_f(samp_rate, analog.GR_COS_WAVE, fm, Am, 0, 0)
+        self.ModuladorAM_0 = ModuladorAM(
             Ac=Ac,
             ka=ka,
         )
-        self.analog_sig_source_x_0 = analog.sig_source_f(samp_rate, analog.GR_COS_WAVE, Fm, A, 0, 0)
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_sig_source_x_0, 0), (self.moduladorAMV2_0, 0))
-        self.connect((self.moduladorAMV2_0, 0), (self.qtgui_freq_sink_x_0, 0))
-        self.connect((self.moduladorAMV2_0, 0), (self.qtgui_time_sink_x_0, 0))
-        self.connect((self.moduladorAMV2_0, 0), (self.uhd_usrp_sink_0, 0))
+        self.connect((self.ModuladorAM_0, 0), (self.qtgui_freq_sink_x_0, 0))
+        self.connect((self.ModuladorAM_0, 0), (self.qtgui_time_sink_x_0, 0))
+        self.connect((self.analog_sig_source_x_0, 0), (self.ModuladorAM_0, 0))
 
 
     def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "MODULADORAM")
+        self.settings = Qt.QSettings("GNU Radio", "modulacionAM")
         self.settings.setValue("geometry", self.saveGeometry())
         self.stop()
         self.wait()
@@ -252,54 +225,39 @@ class MODULADORAM(gr.top_block, Qt.QWidget):
         self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
         self.qtgui_freq_sink_x_0.set_frequency_range(0, self.samp_rate)
         self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
-        self.uhd_usrp_sink_0.set_samp_rate(self.samp_rate)
 
     def get_ka(self):
         return self.ka
 
     def set_ka(self, ka):
         self.ka = ka
-        self.moduladorAMV2_0.set_ka(self.ka)
+        self.ModuladorAM_0.set_ka(self.ka)
 
-    def get_fc(self):
-        return self.fc
+    def get_fm(self):
+        return self.fm
 
-    def set_fc(self, fc):
-        self.fc = fc
-        self.uhd_usrp_sink_0.set_center_freq(self.fc*1e6, 0)
+    def set_fm(self, fm):
+        self.fm = fm
+        self.analog_sig_source_x_0.set_frequency(self.fm)
 
-    def get_GTX(self):
-        return self.GTX
+    def get_Am(self):
+        return self.Am
 
-    def set_GTX(self, GTX):
-        self.GTX = GTX
-        self.uhd_usrp_sink_0.set_gain(self.GTX, 0)
-
-    def get_Fm(self):
-        return self.Fm
-
-    def set_Fm(self, Fm):
-        self.Fm = Fm
-        self.analog_sig_source_x_0.set_frequency(self.Fm)
+    def set_Am(self, Am):
+        self.Am = Am
+        self.analog_sig_source_x_0.set_amplitude(self.Am)
 
     def get_Ac(self):
         return self.Ac
 
     def set_Ac(self, Ac):
         self.Ac = Ac
-        self.moduladorAMV2_0.set_Ac(self.Ac)
-
-    def get_A(self):
-        return self.A
-
-    def set_A(self, A):
-        self.A = A
-        self.analog_sig_source_x_0.set_amplitude(self.A)
+        self.ModuladorAM_0.set_Ac(self.Ac)
 
 
 
 
-def main(top_block_cls=MODULADORAM, options=None):
+def main(top_block_cls=modulacionAM, options=None):
 
     if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
         style = gr.prefs().get_string('qtgui', 'style', 'raster')
